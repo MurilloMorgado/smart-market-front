ed9a7a20d56d431ff1efc26e7af6ad1b606c768a
Murillo M <mumorgado@hotmail.com>
2025-07-31 18:52:05 -0300

    implementando a sessao principal da apresentacao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home.component';
+import { Sobre } from './pages/sobre/sobre.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
+  { path: 'inicio', component: Home },
+  { path: 'sobre', component: Sobre },
 ];
--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from "../../components/header/header.component";
+import { Footer } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-sobre',
   standalone: true,
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './sobre.html',
   styleUrl: './sobre.scss'
 })
