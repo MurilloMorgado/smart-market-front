172692f0b487e6bb19b0c8041ece244f2d1f18dc
Murillo M <mumorgado@hotmail.com>
2025-07-31 12:45:56 -0300

    Criando os components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home.component';
 
 export const routes: Routes = [
-  {}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from "../../components/header/header.component";
+import { Footer } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
