20d9ff332a6f51870073e867d02e7c0c553bc902
Murillo M <mumorgado@hotmail.com>
2025-08-08 11:13:07 -0300

    implementado os cards reaproveitando a estilziacao dos cards no home

---
+++ src/app/model/produto.ts
@@ -0,0 +1,6 @@
+export class Produto {
+  nome!: string;
+  quantidade!: string;
+  imagem!: string;
+  adicionar!: boolean;
+}
--- src/app/pages/lista/lista.component.ts
+++ src/app/pages/lista/lista.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { Header } from "../../components/header/header.component";
+import { Component, OnInit } from '@angular/core';
 import { Footer } from "../../components/footer/footer.component";
+import { Header } from "../../components/header/header.component";
+import { first, firstValueFrom } from 'rxjs';
+import { Produto } from '../../model/produto';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-lista',
@@ -9,6 +12,22 @@ import { Footer } from "../../components/footer/footer.component";
   templateUrl: './lista.html',
   styleUrl: './lista.scss'
 })
-export class Lista {
+export class Lista implements OnInit {
+
+  listaDeProdutos: Produto[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.carregarListaDeProdutos();
+  }
+
+  async carregarListaDeProdutos() {
+    try {
+      this.listaDeProdutos = await firstValueFrom(this.http.get<Produto[]>('data/listaDeProduto.json'));
+    } catch (error) {
+      console.log("Não foi possivel carregar a lista de produtos", error);
+    }
+  }
 
 }
